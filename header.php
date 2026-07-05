27214bf5473f14e9eca6feaf4eaebfb5b2707b58
GINGER <chenyijyun@gmail.com>
2026-07-05 12:09:32 +0800

    perf: 移除 GTM + 延後載入其他第三方追蹤
    - 移除 GTM (GTM-WC755MQ) 完整區塊 + noscript iframe
      原因：GA4 已透過直接 gtag 載入，GTM 只是把 GA4 再載一次
      造成雙重計算 + 173KB 額外 unused JS
    - 保留直接 GA4 gtag（唯一分析源）
    - FB Pixel + LINE Tag + AdSense 改成延後載入：
      * 使用者第一次互動就觸發（scroll/touchstart/mousemove/keydown/click）
      * 或 3 秒 timeout 保底
      * 確保 LCP 之前不會被這些腳本阻塞
    - LINE 的 <noscript> 圖檔追蹤保留（純 HTML fallback）

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
     <?php wp_head(); ?>
-    <!-- Google tag (gtag.js) -->
+    <!-- Google Analytics 4（GA4，唯一分析源；已移除重複的 GTM）-->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-YSVCVM9QTW"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
@@ -17,41 +17,42 @@
         gtag('js', new Date());
         gtag('config', 'G-YSVCVM9QTW');
     </script>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-WC755MQ');</script>
-    <!-- End Google Tag Manager -->
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WC755MQ"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
 
-    <script> !function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '1386137638735275'); fbq('track', 'PageView'); </script>
-
-    <!-- LINE Tag Base Code -->
-    <!-- Do Not Modify -->
+    <!-- 延後載入的第三方追蹤：使用者第一次互動或 3 秒後才觸發，避免阻塞 LCP -->
     <script>
-        (function(g,d,o){
-        g._ltq=g._ltq||[];g._lt=g._lt||function(){g._ltq.push(arguments)};
-        var h=location.protocol==='https:'?'https://d.line-scdn.net':'http://d.line-cdn.net';
-        var s=d.createElement('script');s.async=1;
-        s.src=o||h+'/n/line_tag/public/release/v1/lt.js';
-        var t=d.getElementsByTagName('script')[0];t.parentNode.insertBefore(s,t);
-            })(window, document);
-        _lt('init', {
-        customerType: 'lap',
-        tagId: '299420f4-b7ee-4f96-b243-c4fa8c70f23f'
+    (function(){
+        var loaded = false;
+        function loadDeferred(){
+            if (loaded) return;
+            loaded = true;
+
+            // Facebook Pixel
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1386137638735275');
+            fbq('track', 'PageView');
+
+            // LINE Tag
+            (function(g,d,o){g._ltq=g._ltq||[];g._lt=g._lt||function(){g._ltq.push(arguments)};var h=location.protocol==='https:'?'https://d.line-scdn.net':'http://d.line-cdn.net';var s=d.createElement('script');s.async=1;s.src=o||h+'/n/line_tag/public/release/v1/lt.js';var t=d.getElementsByTagName('script')[0];t.parentNode.insertBefore(s,t);})(window, document);
+            _lt('init', {customerType: 'lap', tagId: '299420f4-b7ee-4f96-b243-c4fa8c70f23f'});
+            _lt('send', 'pv', ['299420f4-b7ee-4f96-b243-c4fa8c70f23f']);
+
+            // AdSense（保留、之後會用；先延後載避免阻塞 LCP）
+            var ad = document.createElement('script');
+            ad.async = true;
+            ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2150625846941263';
+            ad.crossOrigin = 'anonymous';
+            document.head.appendChild(ad);
+        }
+        ['scroll','touchstart','mousemove','keydown','click'].forEach(function(evt){
+            window.addEventListener(evt, loadDeferred, {once: true, passive: true});
         });
-        _lt('send', 'pv', ['299420f4-b7ee-4f96-b243-c4fa8c70f23f']);
+        setTimeout(loadDeferred, 3000);
+    })();
     </script>
     <noscript>
         <img height="1" width="1" style="display:none"
             src="https://tr.line.me/tag.gif?c_t=lap&t_id=299420f4-b7ee-4f96-b243-c4fa8c70f23f&e=pv&noscript=1" />
     </noscript>
-    <!-- End LINE Tag Base Code -->
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined">
@@ -63,7 +64,6 @@
     <!-- 讓 Lottie 主 JS 早點準備 -->
     <link rel="modulepreload" href="<?= esc_url( get_template_directory_uri() ) ?>/js/main.js?20240317">
     <?php endif; ?>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2150625846941263" crossorigin="anonymous"></script>
     <?php $currentUrl = parse_url(home_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH) );?>
 </head>
 
